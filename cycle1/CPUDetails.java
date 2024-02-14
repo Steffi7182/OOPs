8347358a30e4d4195b6dcabcadf3b3c22ca57b27
Steffi Antony <144442545+Steffi7182@users.noreply.github.com>
2024-02-14 16:03:25 +0530

    Add files via upload

---
+++ cycle1/CPUDetails.java
@@ -0,0 +1,35 @@
+class CPU
+{
+	double price;
+	class Processor
+	{
+		double cores;
+		String manufacturer;
+		double getCache()
+		{
+			return 4.3;
+		}
+	}
+	protected class RAM
+	{
+		double memory;
+		String manufacturer;
+		double getClockSpeed()
+		{
+			return 5.5;
+		}
+	}
+}
+public class CPUDetails
+{
+	public static void main(String[] args)
+	{
+        System.out.println(" Steffi Antony\n Roll No: 54\n Date:14-02-2024\n Program title: Information of Processor and RAM.\n");
+	CPU cpu=new CPU();
+	CPU.Processor processor=cpu.new Processor();
+	CPU.RAM ram=cpu.new RAM();
+	System.out.println("Processor Cache=" + processor.getCache());
+	System.out.println("Ram Clock speed=" + ram.getClockSpeed());
+	}
+}
+
